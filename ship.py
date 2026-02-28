078d2c84207d2aa7bf709e6a5d384a1d4449139c
Mithun Prabahar <mithunprabahar@gmail.com>
2026-02-28 15:19:11 +0530

    Update ship.py

--- ship.py
+++ ship.py
@@ -35,11 +35,12 @@ class Ship:
         self.rect.x = self.x
 
     def blitme(self):
-        """Draw the ship at its current location."""
+        #Draw the ship at its current location.
         self.screen.blit(self.image, self.rect)
 
 
     def center_ship(self):
-        """Center the ship on the screen."""
+        #Center the ship on the screen.
         self.rect.midbottom = self.screen_rect.midbottom
         self.x = float(self.rect.x)
+
